06c472c78a6f9c2f8a6441f39cdf404636cb2ba8
Ritesh Gaur <gaur.ritesh@gmail.com>
2019-04-26 19:56:48 +0800

    1st upload

---
+++ modules/hello/src/HelloController/HelloController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\hello\HelloController;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Defines HelloController class.
+ */
+class HelloController extends ControllerBase {
+
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function content() {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Hello, World!'),
+    
+    ];
+  }
+
+}
+
+
+
+
+
+
